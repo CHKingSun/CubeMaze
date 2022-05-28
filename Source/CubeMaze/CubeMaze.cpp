99e14a1997ca9413128af0308009d9ced391ee96
KingSun <chkingsun@gmail.com>
2022-05-28 14:11:03 +0800

    添加项目

---
+++ Source/CubeMaze/CubeMaze.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CubeMaze.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CubeMaze, "CubeMaze" );
---
+++ Source/CubeMaze/CubeMazeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubeMazeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBEMAZE_API ACubeMazeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
